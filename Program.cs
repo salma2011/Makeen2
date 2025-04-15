dc862985ecf599c87d307cfc02ea8cfd3cfdb2c9
Salma <Salmaalalawi2011@gmail.com>
2025-04-15 14:01:14 +0400

    Day1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string fullname = "Salma" + "" + "Alalawi";
+            //string name = "Matar";
+            //int age = 12;
+            //Console.WriteLine(fullname);
+            //Console.WriteLine("hello"+fullname);
+            //Console.WriteLine(name);
+            //var s = 5;
+
+            //s = 30.3;
+
+            //dynamic x = 88;
+            //x=87;
+
+            //nsole.WriteLine(false
+            string s1 = "salma";
+            string s2 = "salma";
+            Console.WriteLine(s1.Equals(s2
+));
+            int x = 70;
+            int mark = 30;
+            var resulate = (mark >= x) ? "Pass" : "fail";
+            Console.WriteLine(resulate);
+        }
+    }
+}
